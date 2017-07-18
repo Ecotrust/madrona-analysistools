1e988aee786665b36264117a7204651ae05df9ba
David Pollard <pollardld@gmail.com>
2017-07-18 09:49:09 -0700

    update for python 3

--- analysistools/urls.py
+++ analysistools/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, url
+from . import views
 
-urlpatterns = patterns('madrona.analysistools.views',
+urlpatterns = [
     url(r'^(?P<uid>[\w_]+)/progress.json$', 'progress', name='analysis-progress'),
     url(r'^(?P<uid>[\w_]+)/progress.html$', 'progress_html', name='analysis-html-progress'),
-)
+]
